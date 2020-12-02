dd8f00e3859dfc6d5669b8b5e7aff403678c2554
Andrzej Mrozik <andrzej.mrozik98@gmail.com>
2020-12-02 16:47:06 +0100

    poprawki menu.py

--- menu.py
+++ menu.py
@@ -7,70 +7,48 @@ from os import listdir
 from os.path import isfile, join
 
 
-def hello_call_back():
-    print("Hello")
+class Menu(tk.Frame):
+    def __init__(self, parent, controller):
 
+        # getting window size
+        window_size = [self.winfo_screenwidth(), self.winfo_screenheight()]
+        # print("Window size: ", window_size[0], "x", window_size[1])
 
-def go_to_new_session(window):
-    window.destroy()
-    newSession.begin()
+        # Some window settings
+        # Example menu see also Menubutton for more flexible menu
+        # To make sub menu you need to add_cascade(label="", menu=menuNameToAddTo)
+        menu = tk.Menu()
+        menu.add_command(label="Continue last session", command=self.hello_call_back)
+        menu.add_command(label="New session", command=self.hello_call_back)
+        menu.add_command(label="Load session", command=self.hello_call_back)
+        menu.add_command(label="Settings", command=self.hello_call_back)
+        menu.add_separator()
+        menu.add_command(label="Exit", command=self.shutdown)
+        self.config(menu=menu)
 
+        # Displaying menu image
+        # TODO: download/make more Menu Images
+        img = Image.open(self.random_image())
+        resize_x = window_size[0] / 3
+        resize_y = window_size[1] / 3
+        img = img.resize((int(resize_x), int(resize_y)), Image.ANTIALIAS)
+        photo_image = ImageTk.PhotoImage(img)
+        menu_image = Canvas(self, width=photo_image.width(), height=photo_image.height())
+        menu_image.pack()
+        menu_image.create_image(1, 1, anchor=NW, image=photo_image)
 
-def random_image():
-    """
-    Searches for files in MenuImages folder and returns random one as path
-    """
-    # TODO: check if chosen file is image
-    images_path = "MenuImages"
-    files = [f for f in listdir(images_path) if isfile(join(images_path, f))]
-    image = random.choice(files)
-    return images_path + "/" + image
+    def hello_call_back(self):
+        print("Hello")
 
+    def random_image(self):
+        """
+        Searches for files in MenuImages folder and returns random one as path
+        """
+        # TODO: check if chosen file is image
+        images_path = "MenuImages"
+        files = [f for f in listdir(images_path) if isfile(join(images_path, f))]
+        image = random.choice(files)
+        return images_path + "/" + image
 
-def shutdown():
-    exit()
-
-
-def begin():
-    # This makes window I guess and starts body of program loop
-    window = tk.Tk()
-
-    # getting window size
-    window_size = [window.winfo_screenwidth(), window.winfo_screenheight()]
-    # print("Window size: ", window_size[0], "x", window_size[1])
-
-    # Some window settings
-    # Window name
-    window.title("DND Masters")
-
-    # Resolution (not set for auto scale)
-    # window.attributes('-fullscreen', True)
-    # window.geometry('1280x720')
-
-    # Example menu see also Menubutton for more flexible menu
-    # To make sub menu you need to add_cascade(label="", menu=menuNameToAddTo)
-    menu = tk.Menu()
-    menu.add_command(label="Continue last session", command=hello_call_back)
-    menu.add_command(label="New session", command=lambda:go_to_new_session(window))
-    menu.add_command(label="Load session", command=hello_call_back)
-    menu.add_command(label="Settings", command=hello_call_back)
-    menu.add_separator()
-    menu.add_command(label="Exit", command=shutdown)
-    window.config(menu=menu)
-
-    # Displaying menu image
-    # TODO: download/make more Menu Images
-    img = Image.open(random_image())
-    resize_x = window_size[0] / 3
-    resize_y = window_size[1] / 3
-    img = img.resize((int(resize_x), int(resize_y)), Image.ANTIALIAS)
-    photo_image = ImageTk.PhotoImage(img)
-    menu_image = Canvas(window, width=photo_image.width(), height=photo_image.height())
-    menu_image.pack()
-    menu_image.create_image(1, 1, anchor=NW, image=photo_image)
-
-    # Here tkinter halts main loop of program (see also update() and update_idletasks())
-    window.mainloop()
-
-
-begin()
+    def shutdown(self):
+        exit()
