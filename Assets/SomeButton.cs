c78fc4cd7d96aff557ccd30e178bf9a0059da424
AMrozik <andrzej.mrozik98@gmail.com>
2021-01-07 18:43:49 +0100

    resolution fix, window no more fullscreen, JD button

---
+++ Assets/SomeButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SomeButton : MonoBehaviour
+{
+    public void JD()
+    {
+        Debug.Log("JDJDJDJDJDJD");
+    }
+}
